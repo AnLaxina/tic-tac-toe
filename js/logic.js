06cffa12b57977a57ec27f29cd14d82d18268bef
AnLaxina <anilovlaxina@gmail.com>
2025-02-26 13:18:56 -0800

    Add functions to change position of board & fix getBoardState

--- js/logic.js
+++ js/logic.js
@@ -7,8 +7,13 @@ const gameBoard = (function () {
         [0, 1, 0]];
 
     const getBoardPosition = (row, column) => board[row][column];
-    const getBoardState = () => board;
-
+    const getBoardState = () => {
+        let boardState = "";
+        for (let row = 0; row < board.length; row++) {
+            boardState += getRow(row).join("|") + "\n";
+        }
+        console.log(boardState);
+    };
     const getRow = (row) => board[row];
     const getColumn = (column) => {
         const verticalNums = [];
@@ -17,13 +22,22 @@ const gameBoard = (function () {
         }
         return verticalNums;
     };
+    const changePosition = (row, column, value) => {
+        board[row][column] = value;
+    };
 
-    return { getBoardPosition, getBoardState, getRow, getColumn };
+    return { getBoardPosition, getBoardState, getRow, getColumn, changePosition };
+})();
+
+// This IIFE module pattern handles all the game logic itself. It is NOT concerned with player input right now
+const game = (function () {
+
 })();
 
 
-
-console.log(gameBoard.getBoardPosition(0, 0));
-console.log(gameBoard.getBoardState());
-console.log(gameBoard.getRow(2));
-console.log(gameBoard.getColumn(1));
+console.log(`The value at [0][0] is: ${gameBoard.getBoardPosition(0, 0)}`);
+console.log("Now it's changing...")
+gameBoard.changePosition(2, 2, "X");
+console.log(`Now the value at [0][0] is: ${gameBoard.getBoardPosition(0, 0)}`);
+console.log(`Now the board state is:`);
+gameBoard.getBoardState();
