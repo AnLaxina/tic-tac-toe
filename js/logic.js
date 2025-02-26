6efd8e5bf90bfe60091a8bf14ac7a808255c2424
AnLaxina <anilovlaxina@gmail.com>
2025-02-26 12:47:17 -0800

    Add functions retrieve the game board's rows and columns

--- js/logic.js
+++ js/logic.js
@@ -4,14 +4,26 @@ const gameBoard = (function () {
     const board =
         [[0, 0, 0],
         [0, 0, 0],
-        [0, 0, 0]];
+        [0, 1, 0]];
 
-    const sayHello = (greeting) => console.log(`${greeting}, nice to meet you!`);
     const getBoardPosition = (row, column) => board[row][column];
+    const getBoardState = () => board;
 
-    return { sayHello, getBoardPosition };
+    const getRow = (row) => board[row];
+    const getColumn = (column) => {
+        const verticalNums = [];
+        for (let row = 0; row < board.length; row++) {
+            verticalNums.push(board[row][column]);
+        }
+        return verticalNums;
+    };
+
+    return { getBoardPosition, getBoardState, getRow, getColumn };
 })();
 
 
-gameBoard.sayHello("Hi!");
+
 console.log(gameBoard.getBoardPosition(0, 0));
+console.log(gameBoard.getBoardState());
+console.log(gameBoard.getRow(2));
+console.log(gameBoard.getColumn(1));
