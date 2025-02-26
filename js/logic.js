f8f5d408d36853f44680f114d720349a817a7ec0
AnLaxina <anilovlaxina@gmail.com>
2025-02-26 13:35:02 -0800

    Add getDiagonal function to retrieve the values in the diagonal direction

--- js/logic.js
+++ js/logic.js
@@ -21,10 +21,10 @@ const gameBoard = (function () {
         console.log(boardState);
     };
 
-    // Gets an Array of values that corresponds with a given row
+    // Returns an Array of values that corresponds with a given row
     const getRow = (row) => board[row];
 
-    // Gets an Array of values that corresponds with a given column
+    // Returns an Array of values that corresponds with a given column
     const getColumn = (column) => {
         const verticalNums = [];
         for (let row = 0; row < board.length; row++) {
@@ -33,12 +33,26 @@ const gameBoard = (function () {
         return verticalNums;
     };
 
+    // Returns an Array of values diagonally
+    // Has one parameter: startPosition that retrieves values based on:
+    // "left" means to take values starting from the top left grid to the bottom right grid
+    // "right" means to take values starting from the top right grid to the bottom left grid
+    const getDiagonal = (startPosition) => {
+        // Since we know that tic-tac-toe has a fixed sized grid, we don't really have to loop through it
+        switch (startPosition) {
+            case "left":
+                return [getCellValue(0, 0), getCellValue(1, 1), getCellValue(2, 2)];
+            case "right":
+                return [getCellValue(0, 2), getCellValue(1, 1), getCellValue(2, 0)];
+        }
+    }
+
     // Changes the position of a cell based on a given row, column, and value
     const changePosition = (row, column, value) => {
         board[row][column] = value;
     };
 
-    return { getCellValue, getBoardState, getRow, getColumn, changePosition };
+    return { getCellValue, getBoardState, getRow, getColumn, getDiagonal, changePosition };
 })();
 
 // This IIFE module pattern handles all the game logic itself. It is NOT concerned with player input right now
@@ -47,5 +61,11 @@ const game = (function () {
 })();
 
 
+gameBoard.changePosition(0, 0, "X");
+gameBoard.changePosition(1, 1, "X");
+gameBoard.changePosition(0, 2, "X");
+gameBoard.changePosition(2, 0, "X");
 console.log(`The board state is:`);
 gameBoard.getBoardState();
+console.log(`The diagonal starting from the top left is: ${gameBoard.getDiagonal("left")}`)
+console.log(`The diagonal starting from the top right is: ${gameBoard.getDiagonal("right")}`)
